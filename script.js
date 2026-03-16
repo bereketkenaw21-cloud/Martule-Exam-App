e612ca680738b08daf3806af313516f8eab6da02
bereketkenaw21-cloud <bereketkenaw21@gmail.com>
2026-03-16 23:20:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// ገጾችን ለማቀያየር የሚረዳ ተግባር
+// ገጾችን ለማቀያየር የሚረዳ ዋና ተግባር
 function showPage(pageId) {
     document.querySelectorAll('.page').forEach(page => page.classList.add('hidden'));
     document.getElementById(pageId).classList.remove('hidden');
@@ -9,8 +9,16 @@ function checkAdmin() {
     let code = prompt("እባክዎ የአስተዳደር ኮድ ያስገቡ:");
     if(code === "12161921") {
         alert("ወደ አስተዳደር ገጽ እየገቡ ነው...");
-        // ወደ አስተዳደር ገጽ የመሄጃ ኮድ እዚህ ይገባል
+        // እዚህ ጋር ወደ አስተዳደር ፓናል የሚወስድ ገጽ (page) እንፈጥራለን
+        showPage('admin-panel'); 
     } else {
         alert("የተሳሳተ ኮድ!");
     }
+}
+
+// የተማሪ የትምህርት ዘርፍ ምርጫ ተግባር
+function selectStream(stream) {
+    alert(stream + " ዘርፍን መረጡ። ወደ ሳብጀክቶች ገጽ እየወሰድንዎት ነው...");
+    // ወደ ሳብጀክቶች ገጽ የሚወስድ ኮድ
+    showPage('subject-list'); 
 }
