bfa79944ec3543200abdfa7af13b44fcb5a6712f
bereketkenaw21-cloud <bereketkenaw21@gmail.com>
2026-03-17 00:29:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
+const CACHE_NAME = 'martule-cache-v1';
+const ASSETS = [
+  '/Martule-Exam-App/',
+  '/Martule-Exam-App/index.html',
+  '/Martule-Exam-App/style.css',
+  '/Martule-Exam-App/script.js'
+];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('martule-store').then((cache) => {
-      return cache.addAll(['/', '/index.html', '/style.css', '/script.js']);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
