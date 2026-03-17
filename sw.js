8000a5032f574f5c148ddcf4c09163e30072f318
bereketkenaw21-cloud <bereketkenaw21@gmail.com>
2026-03-17 16:14:03 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,3 @@
-const CACHE_NAME = 'martule-cache-v1';
-const ASSETS = [
-  '/Martule-Exam-App/',
-  '/Martule-Exam-App/index.html',
-  '/Martule-Exam-App/style.css',
-  '/Martule-Exam-App/script.js'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
